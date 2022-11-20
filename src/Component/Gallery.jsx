788cdf866dea8a2038d35af984eb5e4e08737a43
Sourabh <developrr.sourabh@gmail.com>
2022-11-20 01:36:40 -0600

    add new photo

--- src/Component/Gallery.jsx
+++ src/Component/Gallery.jsx
@@ -16,14 +16,14 @@ const Gallery = () => {
         <div className="portfolio_container layout_padding2">
           <div className="box-1">
             <div className="img-box b-1">
-              <img src="images/p-1.jpg" alt="" />
+              <img src="https://scontent-del1-1.xx.fbcdn.net/v/t39.30808-6/274344911_103298992295433_4713454745635628948_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=MrHYuAGu1d0AX9d8WLZ&_nc_ht=scontent-del1-1.xx&oh=00_AfAPNiB6PX8dLdHBAP3yxwASMCDaKD3SvXQX0-CBsNVCdw&oe=637E5C5D" alt="" />
               <div className="btn-box">
                 <a href className="btn-1">
                 </a>
               </div>
             </div>
             <div className="img-box b-2">
-              <img src="images/p-2.jpg" alt="" />
+              <img src="https://scontent-del1-1.xx.fbcdn.net/v/t39.30808-6/275687133_115687791056553_5490610170336203646_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=LvI8fzYC-jwAX-BJg--&tn=LYmK134n7bEoFs_V&_nc_ht=scontent-del1-1.xx&oh=00_AfBHXzr_PXoTWmgP6DvtEP38et-zcJuDb82apVbQ5LFCQw&oe=637CC3C7" alt="" />
               <div className="btn-box">
                 <a href className="btn-1">
                 </a>
@@ -33,7 +33,7 @@ const Gallery = () => {
           <div className="box-2">
             <div className="box-2-top">
               <div className="img-box b-3">
-                <img src="images/p-3.jpg" alt="" />
+                <img src="https://scontent-del1-1.xx.fbcdn.net/v/t39.30808-6/275610658_114994847792514_8262503328088452631_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=zwSx6_6dOJ8AX9poB_G&_nc_ht=scontent-del1-1.xx&oh=00_AfAx4NZ5BRy_l_9z7t9YWLC384uz8HcjojSxCM5L-5n1Gg&oe=637E7947" alt="" />
                 <div className="btn-box">
                   <a href className="btn-1">
                   </a>
@@ -42,7 +42,7 @@ const Gallery = () => {
             </div>
             <div className="box-2-top2">
               <div className="img-box b-4">
-                <img src="images/p-4.jpg" alt="" />
+                <img src="https://scontent-del1-1.xx.fbcdn.net/v/t39.30808-6/316039342_181045591187439_3356458111887081326_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=xKRR4EeRYm8AX_kAXYy&_nc_ht=scontent-del1-1.xx&oh=00_AfBEuk5u11n02TNxmlHlQ0JssvJoyFWu4LP4qEx3qLWLEA&oe=637DF026" alt="" />
                 <div className="btn-box">
                   <a href className="btn-1">
                   </a>
@@ -51,14 +51,14 @@ const Gallery = () => {
             </div>
             <div className="box-2-btm">
               <div className="img-box b-5">
-                <img src="images/p-5.jpg" alt="" />
+                <img src="https://scontent-del1-1.xx.fbcdn.net/v/t39.30808-6/316039342_181045591187439_3356458111887081326_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=xKRR4EeRYm8AX_kAXYy&_nc_ht=scontent-del1-1.xx&oh=00_AfBEuk5u11n02TNxmlHlQ0JssvJoyFWu4LP4qEx3qLWLEA&oe=637DF026" alt="" />
                 <div className="btn-box">
                   <a href className="btn-1">
                   </a>
                 </div>
               </div>
               <div className="img-box b-6">
-                <img src="images/p-6.jpg" alt="" />
+                <img src="https://scontent-del1-1.xx.fbcdn.net/v/t39.30808-6/316039342_181045591187439_3356458111887081326_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=xKRR4EeRYm8AX_kAXYy&_nc_ht=scontent-del1-1.xx&oh=00_AfBEuk5u11n02TNxmlHlQ0JssvJoyFWu4LP4qEx3qLWLEA&oe=637DF026" alt="" />
                 <div className="btn-box">
                   <a href className="btn-1">
                   </a>
